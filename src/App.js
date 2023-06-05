493d449b2b23c6d0f23c6cbbf342f08d9a41ff98
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-06-05 11:51:34 -0300

    Initializing the React project and creating the cabecalho component

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import Home from './pages/Home'
+import './styles/estilosGlobais.scss'
+export default function App() {
+  return <Home />
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+import Cabecalho from "../../components/Cabecalho";
+
+export default function Home(){
+return <Cabecalho/>
+}
