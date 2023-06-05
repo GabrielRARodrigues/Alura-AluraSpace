ed2992d1964c010571958cafc48840399847b8d4
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-06-05 14:19:14 -0300

    Creating Menu and Banner components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,18 @@
-import Cabecalho from "../../components/Cabecalho";
+import Cabecalho from '../../components/Cabecalho'
+import Menu from '../../components/Menu'
+import styles from './Home.module.scss'
+import Banner from '../../components/Banner'
 
-export default function Home(){
-return <Cabecalho/>
+export default function Home() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <Banner />
+        </section>
+      </main>
+    </>
+  )
 }
