2dd81422be3fa700683820899f3bfb3fed191db8
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-06-12 14:44:01 -0300

    Creating Galeria, Tags and Populares components, importing images from Public folder

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import Menu from '../../components/Menu'
 import styles from './Home.module.scss'
 import Banner from '../../components/Banner'
 import Rodape from '../../components/Rodape'
+import Galeria from '../../components/Galeria'
+import Populares from '../../components/Populares'
 
 export default function Home() {
   return (
@@ -13,6 +15,10 @@ export default function Home() {
           <Menu />
           <Banner />
         </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
       </main>
       <Rodape />
     </>
