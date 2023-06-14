1a6485b15e0d364b547517496d8ba9eb196ec0a0
Gabriel Rocha Andrade Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-06-14 11:52:09 -0300

    implementing the filter feature and creating the Botao component

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import Tags from '../Tags'
 import Cards from './Cards'
 import fotos from './fotos.json'
 import styles from './Galeria.module.scss'
 export default function Galeria() {
+  const [itens, setItens] = useState(fotos)
+  const tags = [...new Set(fotos.map(foto => foto.tag))]
+
+  const filtraFotos = tag => {
+    const novasFotos = fotos.filter(foto => foto.tag === tag)
+    setItens(novasFotos)
+  }
+
   return (
     <section className={styles.galeria}>
       <h2>Navegue pela galeria</h2>
-      <Tags />
-      <Cards itens={fotos} styles={styles} />
+      <Tags tags={tags} filtraFotos={filtraFotos} setItens={setItens} />
+      <Cards itens={itens} styles={styles} />
     </section>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
-import Cabecalho from '../../components/Cabecalho'
-import Menu from '../../components/Menu'
 import styles from './Home.module.scss'
-import Banner from '../../components/Banner'
-import Rodape from '../../components/Rodape'
-import Galeria from '../../components/Galeria'
-import Populares from '../../components/Populares'
+
+import Cabecalho from 'components/Cabecalho'
+import Menu from 'components/Menu'
+import Banner from 'components/Banner'
+import Rodape from 'components/Rodape'
+import Galeria from 'components/Galeria'
+import Populares from 'components/Populares'
 
 export default function Home() {
   return (
